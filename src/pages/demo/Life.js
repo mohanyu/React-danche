8e79fd6441b9aa974d1a057aee90bbaf7ac77aba
HanyuMo <mohanyu6@gmail.com>
2018-09-13 16:47:30 +0800

    框架的构建和react生命周期函数的复习
    框架的构建

---
+++ src/pages/demo/Life.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Child from './child';
+
+export default class Life extends React.Component {
+    constructor(props) {
+        super (props);
+        this.state = {
+            count: 0,
+        }
+    }
+
+    handleAdd=()=> {   //箭头函数绑定不需要 bind(this)
+        this.setState({
+            count: this.state.count + 1,
+        })
+    }
+
+    handleClick(){
+        this.setState({     //不使用箭头函数要bind（this）
+            count: this.state.count + 1,
+        })
+    }
+
+
+    render () {
+        let style = {
+            padding: 50
+        }
+
+        // render方法必须要有return，
+        return <div style={style}>
+
+            <p>React生命周期</p>
+            <button onClick = {this.handleAdd}>dianji</button>
+            <button onClick = {this.handleClick.bind(this)}>点击一下</button>
+            <p>{this.state.count}</p>
+
+            <Child name={this.state.count}></Child>
+
+        </div>
+    }
+
+}
