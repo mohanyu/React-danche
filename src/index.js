5b4fd180cebb4da0c1c996efeb08aafb5cf8a14e
jixiaohui <mohanyu6@gmail.com>
2018-09-14 21:30:48 +0800

    admin.js页面主体

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Admin from './admin';
 import Life from '../src/pages/demo/Life'
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Life />, document.getElementById('root'));
+ReactDOM.render(<Admin />, document.getElementById('root'));
 registerServiceWorker();
