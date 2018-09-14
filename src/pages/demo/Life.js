7eb205a7daded82e039b211ed13e9c0875af7437
jixiaohui <mohanyu6@gmail.com>
2018-09-14 21:30:48 +0800

    环境的搭建+Antd+less支持

--- src/pages/demo/Life.js
+++ src/pages/demo/Life.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import Child from './child';
 import './index.less';
+import {Button} from 'antd';
+// import 'antd/dist/antd.css';
 
 export default class Life extends React.Component {
     constructor(props) {
         super (props);
         this.state = {
             count: 0,
-        }
-    }
+        };
+    };
 
     handleAdd=()=> {   //箭头函数绑定不需要 bind(this)
         this.setState({
             count: this.state.count + 1,
-        })
-    }
+        });
+    };
 
     handleClick(){
         this.setState({     //不使用箭头函数要bind（this）
             count: this.state.count + 1,
-        })
-    }
+        });
+    };
 
 
     render () {
-        let style = {
-            padding: 50
-        }
 
         // render方法必须要有return，
         return <div className="content">
 
             <p>React生命周期</p>
+            <Button onClick={this.handleAdd}>antd点击一下</Button>
             <button onClick = {this.handleAdd}>dianji</button>
             <button onClick = {this.handleClick.bind(this)}>点击一下</button>
             <p>{this.state.count}</p>
@@ -39,6 +39,6 @@ export default class Life extends React.Component {
             <Child name={this.state.count}></Child>
 
         </div>
-    } 
+    };
 
-}
+};
